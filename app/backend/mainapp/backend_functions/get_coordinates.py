687ee87f6e5e4633d2d1d145043aad246389002c
rajesh-gupta-14 <rajesh.gupta@dal.ca>
2020-10-03 19:58:49 -0700

    empty functions created;

---
+++ app/backend/mainapp/backend_functions/get_coordinates.py
@@ -0,0 +1,5 @@
+
+
+def load_coordinates():
+	# load the 46 coordinates of california from a file to save time while making predictions 
+	pass
---
+++ app/backend/mainapp/backend_functions/get_weather.py
@@ -0,0 +1,5 @@
+
+
+def get_weather(coordinates):
+	# get the weather of a given location for a particular date 
+	pass
